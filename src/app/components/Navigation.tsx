2c74ee56c917c0856d3438631909db96595b65c6
Raghuveer <raghuveerp25@gmail.com>
2025-08-11 17:11:22 +0530

    logo complete

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link"
 
 const Navigation = () => {
   return (
-    <div>Navigation</div>
+    <nav className="fixed top-0 left-0 right-0 bg-[#003d5d] shadow-md py-4">
+      <div className="max-w-7xl mx-auto px-4">
+        <div className="flex justify-between items-center">
+          <Link href="/" className="flex items-center">
+            <div className="flex items-center">
+              <div className="w-9 h-9 bg-[#edae49] text-[#003d5b] rounded-full flex items-center justify-center font-bold text-xl mr-2">SB</div>
+            </div>          
+          </Link>
+        </div>
+      </div>
+    </nav>
   )
 }
 
