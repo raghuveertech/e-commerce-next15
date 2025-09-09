412badce7fc8db2b83aa94519654e294191556ac
Raghuveer <raghuveerp25@gmail.com>
2025-09-09 17:30:36 +0530

    products liting page complete

--- src/app/(store)/products/page.tsx
+++ src/app/(store)/products/page.tsx
@@ -1,9 +1,46 @@
-const Products = () => {
-  return (
-    <div className="pt-24">
-      <h1>Products Page</h1>
-    </div>
-  )
-}
+import { categories, productsList } from '@/app/data/products';
+import Image from 'next/image';
+import Link from 'next/link';
 
-export default Products;
+export default function ProductsPage() {
+  const getCategoryImage = (slug: string) => {
+    const firstProduct = productsList.find((product) => {
+      return product.category === slug;
+    });
+    return firstProduct?.image || '/images/placeholder.png';
+  }
+  return (
+    <div className='max-w-7xl mx-auto px-4 pt-24 mb-16'>
+      {/* Categories Grid*/}
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6'>
+        {categories.map((category) => (
+          <Link key={category.slug} href="/products" className='group bg-white rounded-xl overflow-hidden shadow-md hover:shadow-lg hover:translate-y-[-5px] border-b-4 relative transition-all' style={{borderColor: category.color }}>            
+            <div className='h-48 relative overflow-hidden bg-gray-100'>
+              <Image 
+                src={getCategoryImage(category.slug)}
+                alt={category.name}
+                fill
+                sizes="(max-width: 786px) 100vw, (max-width: 1200px) 50vw, 25vw"
+                className='object-contain p-4 group-hover:scale-105 transition-transform duration-300'
+              />
+              <div className='absolute bottom-0 left-0 right-0 h-12 bg-gradient-to-t from-white to-transparent'></div>
+            </div>
+            <div className='p-6'>
+              <h3 className='text-xl font-bold mb-2 text-[#003d5b] group-hover:text-[#00798c] transition-colors' style={{color: category.color}}>{category.name}</h3>
+              <p className='text-sm text-[#30638e] mb-4'>{category.description}</p>
+              <div className='flex justify-end items-center text-sm font-medium transition-colors' style={{color: category.color}}>
+                <span className='mr-1'>View Products</span>
+                <i className='bx bx-chevron-right text-xl transform group-hover:translate-x-1 transition-transform'></i>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+      <div className='mt-12 pt-6 border-t border-gray-200'>
+        <Link href="/" className='inline-flex items-center text-[#00798c] hover:text-[#003d5b] font-medium transition-colors'>
+          <i className='bx bx-arrow-back mr-2'></i> Back to Home
+        </Link>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,56 @@
-import Link from "next/link";
-import Footer from "./components/Footer";
-import Navigation from "./components/Navigation";
+import Link from 'next/link';
+import Footer from './components/Footer';
+import Navigation from './components/Navigation';
+import Image from 'next/image';
 
 export default function HomePage() {
   return (
-   <div className="min-h-screen flex flex-col pt-24">
-    <Navigation />
-    <main className="flex-grow">
-      <h1>Welcome to the Home Page</h1>
-      <Link href='/products' className="text-black-500 underline">Go to Products</Link>
-    </main>
-    <Footer />
-   </div>
+    <div className='min-h-screen flex flex-col'>
+      <Navigation />
+      <div className='relative h-[80vh] mb-16 overflow-hidden bg-gradient-to-r from-[#003d5b] to-[#00798c]'>
+        <div className='max-w-7xl mx-auto px-4 pt-24 h-full flex flex-col'>
+          <div className='flex-grow flex flex-col md:flex-row items-center justify-center md:justify-between'>
+            <div className='w-1/2 text-center md:text-left mb-10 md:mb-0 z-10'>
+              <h1 className='text-4xl sm:text-5xl md:text-7xl font-extrabold text-white tracking-tight'>
+                <div className='mb-2'>
+                  Discover <span className='text-[#edae49]'>Premium</span>
+                </div>
+                <div>Products</div>
+              </h1>
+              <p className='text-xl text-white/80 max-w-lg mb-10'>
+                Experience execeptional quality and service with our curated
+                selection of premium products.
+              </p>
+              <div className='flex flex-col sm:flex-row gap-4 justify-center md:justify-start'>
+                <Link
+                  href='/products'
+                  className='flex px-8 py-3 bg-[#d1495b] text-white rounded-full hover:bg-[#d1495b]/90 transition-all transform hover:scale-105 shadow-lg flex-items-center justify-center'
+                >
+                  Shop Now <i className='bx bx-chevron-right m1-1 text-xl'></i>
+                </Link>
+              </div>
+            </div>
+            <div className='w-1/2 relative z-10 flex justify-center md:justify-end'>
+              <Link href='/products' className='relative group cursor-pointer'>
+                <div className='absolute -inset-4 rounded-full bg-white/10 backdrop-blur-sm animate-pulse group-hover:bg-white/20 transition-colors'></div>
+                <div className='bg-white/5 backdrop-blur-md p-8 rounded-2xl shadow-2xl transform rotate-3 group-hover:rotate-0 transition-transform duration-500'>
+                  <Image
+                    src='/images/product001.png'
+                    alt='featured product image'
+                    width={500}
+                    height={400}
+                    className='rounded-lg drop-shadow-2xl object-contain'
+                  />
+                  <div className='absolute -bottom-4 -right-4 bg-[#edae49] text-white px-4 py-2 rounded-full shadow-lg text-sm fount-bold'>
+                    NEW ARRIVAL
+                  </div>
+                </div>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
   );
 }
